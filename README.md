f14dd03922cce55367a38e27f7ad4f103c0c72b2
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-04-02 18:34:39 +0530

    updated and cleanup some code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # leetcode-clone
+
+## Solve 
+ 1 . make the website to auth with their email and their own password
+
+ 
+    useEffect(() => {
+        const auth = getAuth();
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                console.log("user loggin:", currentUser.email); // Debugging log
+                setUser(currentUser);
+            } else {
+                setUser(null);
+            }
+        });
+
+        return () => unsubscribe();
+    }, []);
--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,62 +1,37 @@
-import { getAuth, signInWithPopup, GithubAuthProvider, GoogleAuthProvider, signInWithRedirect, sendSignInLinkToEmail } from "firebase/auth";
-import { useState } from "react";
+import { useEffect, useState } from "react"
+import { onGithubSignin, onGoogleSignin } from "./utils/auth"
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 
-const provider = new GithubAuthProvider();
-
-const OAuth_URL = "https://leetcode-clone-86cb0.firebaseapp.com/__/auth/handler"
 
 export const Signin = () => {
-    const [email , SetEmail] = useState<string | null>("");
-    const [password , Setpassword] = useState<string | null>("");
-  async function onGithubSignin() {
-    const gitAuth = getAuth();
-    
-    try {
-        const result = await signInWithPopup(gitAuth,provider);
-        const user = result.user;
-        console.log(user);
-    }
-    catch(error) {
-        alert("Error while signin with Github");
-        console.error(error);
-    }
-}
-   async function onGoogleSignin() {
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth();
-    try {
-        await signInWithRedirect(auth,provider);
-    }
-    catch(error) {
-        console.error("Error while SignIn:",error);
-    }
+    const [user,setUser] = useState<User | null>(null);
    
-   }
-  const  SigninWithEmail = async (email : string) => {
-      
-     const actionCodeSettings = {
-        url : "http:/localhost:3000",
-     }
-     const auth = getAuth();
-        try {
-            await sendSignInLinkToEmail(auth,email,actionCodeSettings);
-        }
-        catch(error) {
-            alert("error while loggin with email");
-            console.log("error while login with email:",error);
-        }
-
-   }
-  
+    useEffect(()=>{
+        const auth = getAuth();
+       const unsubscribe =  onAuthStateChanged(auth, (currentUser) => {
+             if (currentUser) {
+                console.log("user loggin");
+                setUser(currentUser);
+             } else {
+                setUser(null);
+             }
+        });
+        
+        return () => unsubscribe();
+    },[]);
     return (
-        <div className="flex justify-center space-x-4"> 
-           <button onClick={onGithubSignin}>Github</button>
-           <button onClick={onGoogleSignin}>Google</button> 
-           <div>
-               <input type="text" placeholder="email" onChange={(e) => { SetEmail(e.target.value)}}/>
-               <button onClick={SigninWithEmail(email)}>Signup</button>
-            </div>  
-        </div>
+       <div>
+             <div>
+            
+             </div>
+            <div className="flex justify-center space-x-4"> 
+               <button onClick={onGithubSignin}>Github</button>
+               <button onClick={onGoogleSignin}>Google</button> 
+            </div>
+         <div>
+              { user?  user.email  : "user not loggin"}
+         </div>
+       </div>
     
     )
 }
