4873a51d98be1359131d04fbd38ce1477d8c2592
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-04-05 16:38:44 +0530

    added register and signin route

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,15 +1,14 @@
 import express from 'express';
 import { router } from './routes/route';
-import prisma from '@repo/db';
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
 app.use("/api/v1",router);
 
-const db = prisma
-
-app.listen(port , () => {
-    console.log("Server is running at port " + port);
+app.listen(port , ()=>{
+    console.log(`server running at port ${port}`)
 })
--- apps/backend/src/routes/auth/auth.ts
+++ apps/backend/src/routes/auth/auth.ts
@@ -1,12 +1,83 @@
+import prisma from '@repo/db';
 import express from 'express';
 export const authRouter = express.Router();
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
-authRouter.post("/",async (req,res)=>{
-    const {username,password} = req.body;
-    if(username === "admin" && password === "admin"){
-         res.status(200).json({message:"Login successful"});
-    }else{
-         res.status(401).json({message:"Invalid credentials"});
-         return;
-    }
+const jwt_secret = process.env.JWt_SECRET_KEY || "leetcode_secret_key";
+
+authRouter.post("/register",async (req,res)=>{
+  const {username,password,email} = req.body;
+  if(username == "" || password == "" || email == "") {
+       res.status(401).json({
+          msg : "We need all the details here"
+       })
+  }
+  try {
+     const hashedPassword = await bcrypt.hash(password,10);
+     const user = await prisma.user.create({
+          data : {
+          username : username,
+          password : hashedPassword,
+          email : email,
+          }
+     })
+     if(user) {
+          res.status(200).json({
+               msg : "user created successfully",
+               userId : user.id
+          })
+     }
+     }catch(error){
+          res.status(403).json({
+               msg : "error happened",
+               error : error
+          })
+     }
+})
+
+authRouter.post("/login",async (req,res)=>{
+     const {username , password } = req.body;
+     if(username == "" || password == "") {
+          res.status(401).json({
+             msg : "We need all the details here"
+          })
+          return;
+     }
+     try {
+          const user = await prisma.user.findUnique({
+               where : {
+                    username : username
+               }
+          })
+          if(!user) {
+               res.status(401).json({
+                    msg : "user is not here"
+               })
+               return;
+          }
+          const userPassword = user.password;
+          const comparePass = bcrypt.compare(password,userPassword);
+          if(!comparePass){
+               res.status(401).json({
+                    msg : "user does not provide correct password"
+               })
+               return;
+          }
+       const token = jwt.sign(user,jwt_secret);
+       res.status(200).json({
+          token : token,
+          msg : "user Loggin successfully"
+     })
+
+          
+     }catch(err){
+          res.status(403).json({
+               msg : "error happened",
+               error : err
+          })
+     }
+
 })
