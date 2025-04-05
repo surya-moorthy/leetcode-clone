5a185e2c775b08d92be7dd4e7d9079c6aed4dedf
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-04-05 15:39:20 +0530

    db is connected atlast

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { router } from './routes/route';
+import prisma from '@repo/db';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,6 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api/v1",router);
 
+const db = prisma
 
 app.listen(port , () => {
     console.log("Server is running at port " + port);
